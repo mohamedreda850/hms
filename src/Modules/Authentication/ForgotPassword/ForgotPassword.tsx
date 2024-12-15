28a403d8285a654d89d330d4b39ce25c9e6978f7
Mohamed22Yousri <mohamedyousrisadeq2003@gmail.com>
2024-12-15 17:55:12 +0200

    forgetPassword

--- src/Modules/Authentication/ForgotPassword/ForgotPassword.tsx
+++ src/Modules/Authentication/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,72 @@
-import styles from './ForgotPassword.module.css';
+import { Container } from "@mui/material";
+import styles from "./ForgotPassword.module.css";
+import Stack from "@mui/material/Stack";
+import ForgotPassImg from "./ForgotPassImg";
+import ForgotInput from "./ForgotInput";
+import ForgotTitle from './ForgotTitle';
 
 export default function ForgotPassword() {
   return (
-    <div>ForgotPassword</div>
-  )
+    <>
+    <Stack
+        direction="row"
+        spacing={5}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "stretch",
+        overflow:"hidden"
+        }}
+      >
+        <Stack
+          direction="column"
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: "50%",
+          }}
+        >
+          <Stack
+            direction="column"
+            spacing={5}
+            sx={{
+              alignItems: "flex-start",
+              width: "50%",
+            }}
+          >
+            <ForgotTitle/>
+          </Stack>
+
+          <Stack
+            component="form"
+            direction="column"
+            spacing={3}
+            sx={{
+              justifyContent: "center",
+              alignItems: "stretch",
+              width: "50%",
+              marginTop: "20px",
+            }}
+          >
+            <ForgotInput />
+          </Stack>
+        </Stack>
+        <ForgotPassImg />
+      </Stack>
+        {/* <Stack
+          direction={{ xs: "column", sm: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 10 }}
+          justifyContent={"end"}
+        >
+          <div>
+            <div>
+           <ForgotTitle/>
+            </div>
+            <ForgotInput />
+          </div>
+          
+            <ForgotPassImg />
+          
+        </Stack> */}
+    </>
+  );
 }
