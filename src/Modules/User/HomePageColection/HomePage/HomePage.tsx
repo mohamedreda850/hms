ef7b4dcd85ff07b5476e9bcb87e020193d78836c
MohamedGamalAbdelaal <mohamed.gamal227422@gmail.com>
2024-12-29 04:02:59 -0800

    make change

--- src/Modules/User/HomePageColection/HomePage/HomePage.tsx
+++ src/Modules/User/HomePageColection/HomePage/HomePage.tsx
@@ -1,11 +1,16 @@
 
 import React from 'react'
 import Section5 from '../Section5/Section5'
+import Section3 from '../Section3/Section3';
+import Section4 from '../Section4/Section4';
 
 
 
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return <>
+  home
+  </>
+  ;
 
 }
--- src/Modules/User/HomePageColection/Section4/Section4.tsx
+++ src/Modules/User/HomePageColection/Section4/Section4.tsx
@@ -1,8 +1,129 @@
-import React from 'react'
 
+import { Box, Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+
+import img1 from '../../../../assets/images/sec3-1.png'
+import img2 from '../../../../assets/images/sec3-2.png'
+import img3 from '../../../../assets/images/sec3-3.png'
+import img4 from '../../../../assets/images/sec3-4.png'
 export default function Section4() {
-  return (
-    <div>Section4</div>
-  )
-}
+  return <>
+  <Typography sx={{fontSize:'2rem',fontWeight:'500',lineHeight:'36px', marginBottom:'20px'}}>
+  Houses with beauty backyard
+  </Typography>
+  <div className="section3">
+  <Grid container spacing={2}>
+      
+      <Grid item xs={3}>
+      <Box
+      component="img"
+      src={img2}
+      alt=""
+      sx={{
+        width: '100%', // Example styles
+        height: '100%',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+      }}
+    />
 
+        <Typography sx={{fontSize:'1.25rem',fontWeight:'400',lineHeight:'30px'}}>
+        Green Park
+        </Typography>
+        <Typography sx={{fontSize:'0.9375rem',fontWeight:'300',lineHeight:'22.5px', color:'#B0B0B0'}}>
+        Tangerang, Indonesia
+        </Typography>
+      </Grid>
+      <Grid item xs={3}>
+      <Box
+      component="img"
+      src={img3}
+      alt=""
+      sx={{
+        width: '100%', // Example styles
+        height: '100%',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+      }}
+    />
+
+        <Typography sx={{fontSize:'1.25rem',fontWeight:'400',lineHeight:'30px'}}>
+        Podo Wae
+        </Typography>
+        <Typography sx={{fontSize:'0.9375rem',fontWeight:'300',lineHeight:'22.5px', color:'#B0B0B0'}}>
+        Madiun, Indonesia
+        </Typography>
+      </Grid>
+      <Grid item xs={3}>
+      <Box
+      component="img"
+      src={img4}
+      alt=""
+      sx={{
+        width: '100%', // Example styles
+        height: '100%',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+      }}
+    />
+
+        <Typography sx={{fontSize:'1.25rem',fontWeight:'400',lineHeight:'30px'}}>
+        Silver Rain
+        </Typography>
+        <Typography sx={{fontSize:'0.9375rem',fontWeight:'300',lineHeight:'22.5px', color:'#B0B0B0'}}>
+        Bandung, Indonesia
+        </Typography>
+      </Grid>
+      <Grid item xs={3}>
+      <Box
+      sx={{
+        position: "relative",
+        width: "100%", // Set your desired width
+        height: "100%", // Set your desired height
+        overflow: "hidden",
+      }}
+    >
+      <Box
+      component="img"
+      src={img1}
+      alt=""
+      sx={{
+        width: '100%', // Example styles
+        height: '100%',
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+        borderRadius: "15px",
+        
+      }}
+    />
+     <Typography
+        sx={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          width: "180px",
+          height:'40px',
+          backgroundColor: "#FF498B", // Semi-transparent background
+          color: "#fff",
+          padding: "8px",
+          textAlign: "center",
+          borderBottomLeftRadius:'15px',
+          borderTopRightRadius:'15px',
+          display: "flex", 
+          alignItems: "center", 
+          justifyContent: "center"
+        }}
+      >
+       Popular Choice
+      </Typography>
+      </Box>
+        <Typography sx={{fontSize:'1.25rem',fontWeight:'400',lineHeight:'30px'}}>
+        Cashville
+        </Typography>
+        <Typography sx={{fontSize:'0.9375rem',fontWeight:'300',lineHeight:'22.5px', color:'#B0B0B0'}}>
+        Kemang, Indonesia
+        </Typography>
+      </Grid>
+    </Grid>
+  </div>
+  </>
+}
